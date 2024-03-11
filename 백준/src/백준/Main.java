b9547f65b5b0f01ad55c063c786e2ba2cc46ea41
yongjoon <missjj312@naver.com>
2024-03-11 17:50:13 +0900

    백준코드

---
+++ 백준/src/백준/Main.java
@@ -0,0 +1,26 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int num1 =sc.nextInt();
+		int num2=sc.nextInt();
+		
+		
+		System.out.println(num1*5);
+		System.out.println(num1*8);
+		System.out.println(num1*3);
+		System.out.println(num1*num2);
+		
+		
+		
+		
+		
+	}
+
+}
