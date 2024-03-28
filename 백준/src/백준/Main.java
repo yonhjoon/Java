c722343cb4235d627657dc89139280e1de75e5c5
yongjoon <missjj312@naver.com>
2024-03-28 16:29:37 +0900

    asd

--- 백준/src/백준/Main.java
+++ 백준/src/백준/Main.java
@@ -7,14 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int Long = A /4;
-		
-		for(int i = 0; i < Long; i++) {
-			System.out.print("long ");
-		}      
-		System.out.println("int");
-		                        
 		
 	}
 }
